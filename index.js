9bec1d447e7b354e87cdc31dbefa4d53c5b02e26
Dina alsaid <41339383+dinaAlsaid@users.noreply.github.com>
2022-03-23 12:14:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const server = require('./src/server');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
+const MONGODB_URI = process.env.MONGODB_URI ;
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(MONGODB_URI, {
